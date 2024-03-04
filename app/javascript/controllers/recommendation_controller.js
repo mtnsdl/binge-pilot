ae698e8b72c64fcf05c72a71fc38c0ae3b3c44c4
Viorel Borgovan <viorel.borgovan@gmail.com>
2024-03-04 20:10:23 +0100

    wip

---
+++ app/javascript/controllers/recommendation_controller.js
@@ -0,0 +1,63 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["image", "movieGenre", "tvShowGenre"]
+
+  connect() {
+    console.log("hello from reccomendation controller")
+    }
+
+    selectMovies(event) {
+      event.preventDefault();
+      this.hideInitialOptions();
+      this.movieGenreTargets.forEach((element) => element.classList.remove("hidden"));
+    }
+
+    selectTVShows(event) {
+      event.preventDefault();
+      this.hideInitialOptions();
+      this.tvShowGenreTargets.forEach((element) => element.classList.remove("hidden"));
+    }
+
+    hideInitialOptions() {
+      this.imageTargets.forEach((element) => element.classList.add("hidden"));
+    }
+
+  loadMovies() {
+    const apiKey = "ca66492742d84664bddc760e88bf6b2b";
+    const baseUrl = "https://api.themoviedb.org/3/discover/movie";
+    const queryParams = "include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+    const url = `${baseUrl}?api_key=${apiKey}&${queryParams}`;
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response for movies was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => console.error("There was a problem with the fetch operation in movies: ", error));
+  }
+
+  loadTVShows() {
+    const apiKey = "ca66492742d84664bddc760e88bf6b2b";
+    const baseUrl = "https://api.themoviedb.org/3/discover/tv";
+    const queryParams = "include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+    const url = `${baseUrl}?api_key=${apiKey}&${queryParams}`;
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response for tv shows was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => console.error("There was a problem with the fetch operation in tv shows: ", error));
+  }
+}
