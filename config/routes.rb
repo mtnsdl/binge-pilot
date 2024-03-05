3a8ced57429a8866468f2a9fa6a34b2cd48dbc67
Viorel Borgovan <viorel.borgovan@gmail.com>
2024-03-05 14:33:01 +0100

    removed js from view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+
+#  get "/moviescard", to: "pages#moviescard", as: :moviescard
 # post '/recommendations', to: 'pages#recommendations'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
