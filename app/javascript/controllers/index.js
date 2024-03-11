521b371f684048d1b37d0f5d5f5cbba529074fc0
Martin <155843922+cap89@users.noreply.github.com>
2024-03-11 16:38:10 +0100

    trying of swipe feature

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,20 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+document.addEventListener('DOMContentLoaded', function() {
+  var posterContainer = document.getElementById('swipeable-poster');
+  var hammertime = new Hammer(posterContainer);
+
+  hammertime.on('swipeleft', function(ev) {
+    // Code to trigger when swiped left, setting :liked to false
+    console.log("Swipe left detected");
+    document.getElementById("discard").click(); // Simulate click on "discard" button
+  });
+
+  hammertime.on('swiperight', function(ev) {
+    // Code to trigger when swiped right, setting :liked to true
+    console.log("Swipe right detected");
+    document.getElementById("save-for-later").click(); // Simulate click on "save for later" button
+  });
+});
