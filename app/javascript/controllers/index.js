77719b480d2b4392fa7cc7967b19acbec7a05ef3
Martin <155843922+cap89@users.noreply.github.com>
2024-03-11 15:26:49 +0100

    no javascript needed for this feature

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,26 +9,3 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var container = document.querySelector('.poster-container');
-
-//   function toggleFlip() {
-//       container.classList.toggle('flip');
-
-//       var icon = document.querySelector('.flip-icon'); // Selects the flip icon within the container
-
-//       if (container.classList.contains('flip')) {
-//           icon.src = icon.dataset.flipIconReverse;
-//       } else {
-//           icon.src = icon.dataset.flipIconInfo;
-//       }
-//   }
-
-//   // Listening for both click and touchend events
-//   container.addEventListener('click', toggleFlip);
-//   container.addEventListener('touchend', function(e) {
-//       e.preventDefault(); // Prevents the click event from firing after touchend
-//       toggleFlip();
-//   });
-// });
