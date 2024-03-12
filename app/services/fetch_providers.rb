ffb74b6b516137f371752141b55c8809a288a898
Martin <155843922+cap89@users.noreply.github.com>
2024-03-12 16:26:26 +0100

    change of file name neccessary according to the naming convention3

--- app/services/fetch_providers.rb
+++ app/services/fetch_providers.rb
@@ -1,7 +1,7 @@
 require "open-uri"
 require "nokogiri"
 
-class FetchMovieProviderService
+class FetchProviders
   USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 
   def initialize(content, id, name, random_result_name_parse)
